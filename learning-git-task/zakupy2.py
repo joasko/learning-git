691381d6d4e915d65db58fa140ccd86777dad082
Joanna <skowron.jss@gmail.com>
2022-06-18 09:23:13 +0200

    słownik

--- learning-git-task/zakupy2.py
+++ learning-git-task/zakupy2.py
@@ -2,11 +2,3 @@ sklepy = {
     "warzywniak": ["marchew", "seler", "rukola"],
     "piekarnia": ["chleb", "pączek", "bułki"]
 }
-
-count = 0
-
-for sklep, produkty in sklepy.items():
-    count+= len(produkty)
-    print(f"Idę do {sklep.capitalize()} i kupuję {str(produkty).title()}")
-
-print(f"W sumie: {count}")

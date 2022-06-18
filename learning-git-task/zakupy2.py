67690415379328f42eaaa5ab01b881bd868cce1d
Joanna <skowron.jss@gmail.com>
2022-06-18 09:28:13 +0200

    pętla

--- learning-git-task/zakupy2.py
+++ learning-git-task/zakupy2.py
@@ -2,3 +2,8 @@ sklepy = {
     "warzywniak": ["marchew", "seler", "rukola"],
     "piekarnia": ["chleb", "pączek", "bułki"]
 }
+count = 0
+
+for sklep, produkty in sklepy.items():
+    count+= len(produkty)
+    print(f"Idę do {sklep.capitalize()} i kupuję {str(produkty).title()}")
